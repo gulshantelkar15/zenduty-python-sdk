67ad9370bb5bf31584926676e28ff4b00970929c
Gulshan Telkar <gulshan@zenduty.com>
2024-08-07 14:12:01 +0530

    chore: Update setup.py with latest package versions

--- setup.py
+++ setup.py
@@ -9,6 +9,13 @@ setup(
     author="Vishwa Krishnakumar",
     author_email="vishwa@yellowant.com",
     packages=find_packages(),
-    install_requires=["urllib3", "six==1.9.0"],
+    install_requires=[
+        "requests==2.32.3",
+        "urllib3==2.2.2",
+        "six==1.9.0",
+        "charset-normalizer==3.3.2",
+        "idna==3.7",
+        "certifi==2024.7.4"
+    ],
     scripts=["bin/client.py"],
 )
